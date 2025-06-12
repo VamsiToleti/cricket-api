1b437f898aaf39bec514d3891bb1cdfeab4ac7cb
VamsiToleti <vamsiindia2003@gmail.com>
2025-06-12 14:05:55 +0530

    commit

---
+++ india.js
@@ -0,0 +1,9 @@
+let btn=document.querySelector("button");
+let count=0;
+let p=document.querySelector("p");
+btn.addEventListener("click", function(){
+    count+=1;
+    p.innerText=count;
+    
+
+})
